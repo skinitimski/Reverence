d6d8e18c1df8d86a0f15784e964031cc56902a12
timski <skinitimski@gmail.com>
2015-05-30 21:40:30 -0400

    Do a level up demo

--- UnitTests/Seven/Test.Character.LevelUp.cs
+++ UnitTests/Seven/Test.Character.LevelUp.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using SystemThread = System.Threading.Thread;
 
 using NUnit.Framework;
@@ -13,18 +14,30 @@ namespace Atmosphere.UnitTests.Seven
         [Test]
         public void LevelUpTest()
         {
-            string cloud = LevelUpDemo.GetCharacterStats("Cloud", 45);
-            string tifa = LevelUpDemo.GetCharacterStats("Tifa", 45);
-            string barret = LevelUpDemo.GetCharacterStats("Barret", 45);
+            string cloud = LevelUpDemo.GetCharacterStats("Cloud", 53);
+            string tifa = LevelUpDemo.GetCharacterStats("Tifa", 46);
+            string barret = LevelUpDemo.GetCharacterStats("Barret", 44);
 
-            string aeris = LevelUpDemo.GetCharacterStats("Aeris", 45);
-            string nanaki = LevelUpDemo.GetCharacterStats("RedXIII", 45);
-            string caitsith = LevelUpDemo.GetCharacterStats("CaitSith", 45);
+            string aeris = LevelUpDemo.GetCharacterStats("Aeris", 48);
+            string nanaki = LevelUpDemo.GetCharacterStats("RedXIII", 46);
+            string caitsith = LevelUpDemo.GetCharacterStats("CaitSith", 43);
 
-            string yuffie = LevelUpDemo.GetCharacterStats("Yuffie", 45);            
-            string vincent = LevelUpDemo.GetCharacterStats("Vincent", 45);
-            string cid = LevelUpDemo.GetCharacterStats("Cid", 45);
-            
+            string yuffie = LevelUpDemo.GetCharacterStats("Yuffie", 44);            
+            string vincent = LevelUpDemo.GetCharacterStats("Vincent", 47);
+            string cid = LevelUpDemo.GetCharacterStats("Cid", 47);
+
+            StringBuilder all = new StringBuilder();
+            all.AppendLine(cloud);
+            all.AppendLine(tifa);
+            all.AppendLine(barret);
+            all.AppendLine(aeris);
+            all.AppendLine(nanaki);
+            all.AppendLine(caitsith);
+            all.AppendLine(yuffie);
+            all.AppendLine(vincent);
+            all.AppendLine(cid);
+
+            string a = all.ToString();
         }
     }
 }
